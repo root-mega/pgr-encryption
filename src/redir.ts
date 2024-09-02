238378752fa48ce3124a7095f99eb9637edcd9f0
root-mega <65620837+root-mega@users.noreply.github.com>
2024-09-02 17:36:50 +0200

    redir

--- src/redir.ts
+++ src/redir.ts
@@ -1,64 +1,55 @@
 import { Config } from "./config.js";
 
-export class URL {
-    protocol: string;
-    hostname: string;
-    port: string;
-    pathname: string;
-
-    constructor(url: string) {
-        const urlParts = url.split("://");
-        this.protocol = urlParts[0];
-        const rest = urlParts[1] || '';
-
-        const [hostAndPort, ...pathParts] = rest.split("/");
-        const [host, port] = hostAndPort.split(":");
-        this.hostname = host || '';
-        this.port = port || '';
-        this.pathname = pathParts.join("/") || '';
-    }
-
-    toString(): string {
-        return `${this.protocol}://${this.hostname}${this.port ? ':' + this.port : ''}/${this.pathname}`;
-    }
+function replaceHostAndPort(url: string, newHost: string, newPort: string): string {
+    const urlPattern = /^(https?:\/\/)([^:/\s]+)(:\d+)?(\/.*)?$/;
+    
+    return url.replace(urlPattern, (match, protocol, oldHost, oldPort, path) => {
+        return `${protocol}${newHost}${newPort ? `:${newPort}` : ''}${path || ''}`;
+    });
 }
 
 export class Redir {
-    static SendWebRequestPtr: NativePointer = ptr(0x2AAA860);
-    static CreateStringPtr: NativePointer = ptr(0x1D20A40);
+    static WebRequestSetUrlPtr: NativePointer = ptr(0x2AAE950);
+    static UriCtorPtr: NativePointer = ptr(0x1A8E0E0);
 
     static Il2cppToString(str: NativePointer): string {
         return str.add(20).readUtf16String(str.add(16).readS32())!;
     }
 
     static InitHook(base: NativePointer): void {
-        console.log("[Redir] Started hooks!");
-
-        Interceptor.replace(base.add(this.SendWebRequestPtr), new NativeCallback((a: NativePointer): NativePointer => {
-            const originalUrl = this.Il2cppToString(a);
-            if (originalUrl.includes("icanhazip")) {
-                console.log(`[Redir] SendWebRequest: ${originalUrl}`);
-                return this.SendWebRequest(originalUrl);
-            } else {
-                const url = new URL(originalUrl);
-
-                url.hostname = Config.redirData.ip;
-                url.port = Config.redirData.port;
-
-                const modifiedUrl = url.toString();
-                console.log(`[Redir] SendWebRequest: ${modifiedUrl}`);
-                return this.SendWebRequest(modifiedUrl);
+        Interceptor.attach(base.add(this.WebRequestSetUrlPtr), {
+            onEnter: function(args) {
+                var requestUrl = Redir.Il2cppToString(args[1]);
+                if (requestUrl.startsWith("http://") || requestUrl.startsWith("https://")) {
+                    if (requestUrl.includes(Config.redirData.ip + ":" + Config.redirData.port)) return;
+                    
+                    var prefix = requestUrl.split('/', 3).join('/');
+                    args[1] = Redir.Il2CppNewUtf16(replaceHostAndPort(requestUrl, Config.redirData.ip, Config.redirData.port));
+                    
+                    console.log("[Redir] Successfully redirected " + requestUrl);
+                }
             }
-        }, "pointer", ["pointer"]));
+        })
+
+        Interceptor.attach(base.add(this.UriCtorPtr), {
+            onEnter: function(args) {
+                var requestUrl = Redir.Il2cppToString(args[1]);
+                if (requestUrl.startsWith("http://") || requestUrl.startsWith("https://")) {
+                    if (requestUrl.includes(Config.redirData.ip + ":" + Config.redirData.port)) return;
+                    
+                    var prefix = requestUrl.split('/', 3).join('/');
+                    args[1] = Redir.Il2CppNewUtf16(replaceHostAndPort(requestUrl, Config.redirData.ip, Config.redirData.port));
+                    
+                    console.log("[Redir] Successfully redirected " + requestUrl);
+                }
+            }
+        })
+        
+        console.log("[Redir] Started hooks!");
     }
 
-    static CreateString(string: string): NativePointer {
-        const createStringFunc = new NativeFunction(Process.getModuleByName("GameAssembly.dll").base.add(this.CreateStringPtr), "pointer", ["pointer"]);
-        return createStringFunc(Memory.allocUtf8String(string));
-    }
-
-    static SendWebRequest(uri: string): NativePointer {
-        const sendWebRequestFunc = new NativeFunction(Process.getModuleByName("GameAssembly.dll").base.add(this.SendWebRequestPtr), "pointer", ["pointer"]);
-        return sendWebRequestFunc(this.CreateString(uri));
+    static Il2CppNewUtf16(text: string) {
+        const alloc_string = Memory.allocUtf16String(text);
+        return new NativeFunction(Module.getExportByName("GameAssembly.dll", "il2cpp_string_new_utf16"), 'pointer', ['pointer', 'int'])(alloc_string, text.length)
     }
 }
